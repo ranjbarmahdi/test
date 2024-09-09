bb427ae6b15f7b853aad0b381ad1c77b9f73a7ee
ranjbarmahdi <ranmahdi66@gmail.com>
2024-09-09 08:09:51 -0700

    A

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use((req, res) => {
     </style>
 </head>
 <body>
-    JABBAR
+    <span style="color:red">J</span>A<span style="color:brown">B</span>BA<span style="color:red">B</span>
 </body>
 </html>
 `);
